aaafc0d5406902568a8e8cabf71a243cc65e0572
Dan0227 <jhojan0227@gmail.com>
2024-06-25 00:13:19 -0500

    'nojodas'

--- Server/models/Usuario.js
+++ Server/models/Usuario.js
@@ -12,19 +12,37 @@ const Usuario = {
       id_rol,
       telefono,
       direccion_envio,
-      foto_perfil,
-      google_id,
-      facebook_id,
+      foto_perfil
     } = data;
 
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = contraseña ? await bcrypt.hash(contraseña, salt) : null;
 
-    const [result] = await pool.query(
-      `INSERT INTO usuarios (username, nombre_usuario, apellido_usuario, contraseña, correo_electronico, id_rol, telefono, direccion_envio, foto_perfil, google_id, facebook_id) 
-      VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`,
-      [username, nombre_usuario, apellido_usuario, hashedPassword, correo_electronico, id_rol, telefono, direccion_envio, foto_perfil, google_id, facebook_id]
-    );
+    let query = `
+      INSERT INTO usuarios (username, nombre_usuario, apellido_usuario, contraseña, correo_electronico, id_rol, telefono`;
+
+    let values = [username, nombre_usuario, apellido_usuario, hashedPassword, correo_electronico, id_rol, telefono];
+
+    if (direccion_envio !== undefined) {
+      query += ', direccion_envio';
+      values.push(direccion_envio);
+    }
+
+    if (foto_perfil !== undefined) {
+      query += ', foto_perfil';
+      values.push(foto_perfil);
+    }
+
+    query += ') VALUES (?, ?, ?, ?, ?, ?, ?';
+    if (direccion_envio !== undefined) {
+      query += ', ?';
+    }
+    if (foto_perfil !== undefined) {
+      query += ', ?';
+    }
+    query += ')';
+
+    const [result] = await pool.query(query, values);
 
     return result.insertId;
   },
@@ -49,22 +67,34 @@ const Usuario = {
       id_rol,
       telefono,
       direccion_envio,
-      foto_perfil,
-      google_id,
-      facebook_id,
+      foto_perfil
     } = data;
 
-    let updateQuery = `UPDATE usuarios SET username = ?, nombre_usuario = ?, apellido_usuario = ?, correo_electronico = ?, id_rol = ?, telefono = ?, direccion_envio = ?, foto_perfil = ?, google_id = ?, facebook_id = ?`;
-    const updateValues = [username, nombre_usuario, apellido_usuario, correo_electronico, id_rol, telefono, direccion_envio, foto_perfil, google_id, facebook_id];
+    let updateQuery = `
+      UPDATE usuarios SET
+      username = ?, nombre_usuario = ?, apellido_usuario = ?, correo_electronico = ?, id_rol = ?, telefono = ?
+    `;
+
+    const updateValues = [username, nombre_usuario, apellido_usuario, correo_electronico, id_rol, telefono];
+
+    if (direccion_envio !== undefined) {
+      updateQuery += ', direccion_envio = ?';
+      updateValues.push(direccion_envio);
+    }
+
+    if (foto_perfil !== undefined) {
+      updateQuery += ', foto_perfil = ?';
+      updateValues.push(foto_perfil);
+    }
 
     if (contraseña) {
       const salt = await bcrypt.genSalt(10);
       const hashedPassword = await bcrypt.hash(contraseña, salt);
-      updateQuery += `, contraseña = ?`;
+      updateQuery += ', contraseña = ?';
       updateValues.push(hashedPassword);
     }
 
-    updateQuery += ` WHERE id_usuario = ?`;
+    updateQuery += ' WHERE id_usuario = ?';
     updateValues.push(id);
 
     const [result] = await pool.query(updateQuery, updateValues);
@@ -78,16 +108,6 @@ const Usuario = {
 
   async getUserByEmail(email) {
     const [results] = await pool.query('SELECT * FROM usuarios WHERE correo_electronico = ?', [email]);
-    return results[0];
-  },
-
-  async getUserByGoogleId(google_id) {
-    const [results] = await pool.query('SELECT * FROM usuarios WHERE google_id = ?', [google_id]);
-    return results[0];
-  },
-
-  async getUserByFacebookId(facebook_id) {
-    const [results] = await pool.query('SELECT * FROM usuarios WHERE facebook_id = ?', [facebook_id]);
     return results[0];
   },
 
